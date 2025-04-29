97c611eed9ca40fa317ec8f35f33480a282b5a33
root <root@DESKTOP-VK6DJID.>
2025-04-29 13:29:13 +0200

    Refaktoryzacja

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Depends
+from contextlib import asynccontextmanager
+from app.util.init_db import create_tables
+from app.routers.auth import authRouter
+from app.util.protectRoute import get_current_user
+from app.db.schema.user import UserOutput
+
+@asynccontextmanager
+async def lifespan(app : FastAPI):
+    print("Created")
+    create_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router=authRouter, tags=["autentykacja"], prefix="/auth")
+
+@app.get("/health")
+def health_check():
+    return{"status": "Running..."}
+
+@app.get("/protected")
+def read_protected(user: UserOutput = Depends(get_current_user)):
+    return{"status": "Running..."}
